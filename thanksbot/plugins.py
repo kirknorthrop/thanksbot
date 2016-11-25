42ff2ae0a2e95c5c602da65d9564d6e68a9aac00
Kirk Northrop <kirk@oldstlabs.com>
2016-11-25 12:32:02 +0000

    Deal better with vowels

--- thanksbot/plugins.py
+++ thanksbot/plugins.py
@@ -3,18 +3,18 @@ import re
 from slackbot.bot import listen_to, respond_to
 
 
-@respond_to('thanks,? ([^<>:\'\";]*)', re.I)
-@listen_to('thanks,? ([^<>:\'\";]*)', re.I)
+@respond_to('.*thanks,? ([^<>:\'\";]*)', re.I)
+@listen_to('.*thanks,? ([^<>:\'\";]*)', re.I)
 def give_thanks(message, something):
     # This algorithm comes from https://github.com/jzplusplus/ThanksAnts
     # Thanks Jay Zuerndorfer.
     # Thuerndorfer.
 
-    vowels = 'aeiouy'
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'qu']
 
     if something:
         for i, letter in enumerate(something):
-            if letter in vowels:
+            if letter in vowels and i > 0:
                 break
 
         if i > 0 and i < len(something):
